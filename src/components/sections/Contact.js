2a70a949f1bb808fd45044c8c19c2c77573c3c28
Claudia Diosan <mcd1591@gmail.com>
2021-02-22 01:06:54 +0100

    contact section finished

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -13,6 +13,7 @@ export default () => (
     <div>
       <h1>Contact</h1>
         <Formik name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field"
+        action={process.env.GATSBY_PORTFOLIO_FORMIK_ENDPOINT}
     initialValues={{
       name: '',
       email: '',
