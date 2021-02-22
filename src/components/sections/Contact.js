22c69108a55b47a9ecc0aec4d1909307b8b763e5
Claudia Diosan <mcd1591@gmail.com>
2021-02-22 14:48:49 +0100

    contact form finished

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Formik, Field } from 'formik'
-import { navigate } from 'gatsby'
+import { Formik, Field} from 'formik'
 import * as Yup from 'yup';
+import styled from 'styled-components';
+import { Section, Container } from '@components/global';
+import Contact from '@images/contact/contact.png'
 
 const encode = (data) => {
   return Object.keys(data)
@@ -11,6 +13,10 @@ const encode = (data) => {
 
 const ContactForm = () => {
   return (
+    <Section id="contact" accent="primary">
+      <StyledContainer>
+      <div>
+      <h1>Contact</h1>
     <Formik
       initialValues={{ name: '', email: '', message: '' }}
       validationSchema={validationSchema}
@@ -24,7 +30,6 @@ const ContactForm = () => {
           }),
         })
           .then(() => {
-            navigate('/contact/success')
             setSubmitting(false)
           })
           .catch(error => {
@@ -46,6 +51,7 @@ const ContactForm = () => {
         data-netlify='true'
         data-netlify-honeypot='bot-field'
       >
+
         <div className='field'>
           <label className='label'>Name</label>
           <div className='control'>
@@ -69,23 +75,49 @@ const ContactForm = () => {
           </div>
           {touched.message && errors.message && <small className='has-text-danger'>{errors.message}</small>}
         </div>
-
-        <div className='field is-grouped is-pulled-right'>
-          <div className='control'>
-            <button className='button' type='reset'>Clear</button>
-          </div>
-          <div className='control'>
-            <button className='button is-primary' type='submit' disabled={isSubmitting}>Submit</button>
-          </div>
+        <div>
+          <ContactButton secondary type="submit" disabled={isSubmitting}>Send</ContactButton>
         </div>
       </form>)}
     />
+    </div>
+  <Art>
+  <img src={Contact} alt="Contact image" />
+  </Art>
+      </StyledContainer>
+    </Section>
   )
 }
 
 export default ContactForm;
 
 
+const ContactButton = styled.button`
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 0.7rem 2.5rem;
+  border: none;
+  -webkit-appearance: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background: #0074d9;
+  &:focus {
+    outline: none;
+  }
+  &:disabled {
+    background: gray;
+  }
+  ${({ secondary }) =>
+    secondary &&
+    `
+		background: #001F3F;
+	`}
+`;
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
@@ -98,3 +130,33 @@ const validationSchema = Yup.object().shape({
   message: Yup.string()
     .required('Message is Required!'),
 })
+
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  
+  @media (max-width: ${props => props.theme.screen.md}) {
+    justify-content: center;
+  }
+`;
+
+
+const Art = styled.figure`
+  width: 600px;
+  position: absolute;
+  top: 20%;
+  right: 50%;
+
+  @media (max-width: ${props => props.theme.screen.lg}) {
+    top: 0;
+    right: 65%;
+    width: 500px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    display: none;
+  }
+`;
