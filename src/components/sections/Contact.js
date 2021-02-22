f49ae7845640f5a1c659006a2b6871868fb49344
Claudia Diosan <mcd1591@gmail.com>
2021-02-22 13:52:07 +0100

    contact form changed

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,99 +1,214 @@
-import React from 'react'
-import { Formik, Field } from 'formik'
-import { navigate } from 'gatsby'
+import React from 'react';
+import axios from 'axios';
+import { Formik, Form, FastField, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
+import { Section, Container } from '@components/global';
+import Contact from '@images/contact/contact.png'
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
 
-const ContactForm = () => {
-  return (
-    <Formik
-      initialValues={{ name: '', email: '', message: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
+export default () => (
+  <Section id="contact" accent="secondary">
+    <StyledContainer>
+    <div>
+      <h1>Contact</h1>
+        <Formik 
+    initialValues={{
+      name: '',
+      email: '',
+      message: '',
+      success: false,
+    }}
+    validationSchema={Yup.object().shape({
+      name: Yup.string().required('Full name field is required'),
+      email: Yup.string()
+        .email('Invalid email')
+        .required('Email field is required'),
+      message: Yup.string().required('Message field is required'),
+    })}
+    onSubmit={async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
+       
+      try {
         fetch("/?no-cache=1", {                                 //eslint-disable-line
           method: 'POST',
           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: encode({
-            'form-name': 'contact',
-            ...values,
+          data: JSON.stringify({
+            name,
+            email,
+            message,
           }),
         })
           .then(() => {
-            setSubmitting(false)
+            setSubmitting(false);
+            setFieldValue('success', true);
+            setTimeout(() => resetForm(), 6000);
           })
-          .catch(error => {
-            console.log(error)
-            alert("Error: Please Try Again!");                            //eslint-disable-line
-            setSubmitting(false)
-          })
-      }}
-      render={({
-        errors,
-        touched,
-        isSubmitting,
-        handleSubmit,
-        handleReset,
-      }) => (<form
-        name='contact'
-        onSubmit={handleSubmit}
-        onReset={handleReset}
-        data-netlify='true'
-        data-netlify-honeypot='bot-field'
-      >
-        <div className='field'>
-          <label className='label'>Name</label>
-          <div className='control'>
-            <Field className='input' type='text' placeholder='Full Name' name='name' id='name' />
-          </div>
-          {touched.name && errors.name && <small className='has-text-danger'>{errors.name}</small>}
-        </div>
+      } catch (err) {
+        setSubmitting(false);
+        setFieldValue('success', false);
+				alert('Something went wrong, please try again!') // eslint-disable-line
+      }
+    }}
+  >
+    {({ values, touched, errors, setFieldValue, isSubmitting }) => (
+      <Form  name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field">
+         <input type="hidden" name="form-name" value="contact" />
+         <input type="hidden" name="bot-field" />
+        <InputField>
+          <Input
+            as={FastField}
+            type="text"
+            name="name"
+            component="input"
+            aria-label="name"
+            placeholder="Full name*"
+            error={touched.name && errors.name}
+          />
+          <ErrorMessage component={Error} name="name" />
+        </InputField>
+        <InputField>
+          <Input
+            id="email"
+            aria-label="email"
+            component="input"
+            as={FastField}
+            type="email"
+            name="email"
+            placeholder="Email*"
+            error={touched.email && errors.email}
+          />
+          <ErrorMessage component={Error} name="email" />
+        </InputField>
+        <InputField>
+          <Input
+            as={FastField}
+            component="textarea"
+            aria-label="message"
+            id="message"
+            rows="8"
+            type="text"
+            name="message"
+            placeholder="Message*"
+            error={touched.message && errors.message}
+          />
+          <ErrorMessage component={Error} name="message" />
+        </InputField>
+        {values.success && (
+          <InputField>
+            <Center>
+              <h4>Your message has been successfully sent, I will get back to you ASAP!</h4>
+            </Center>
+          </InputField>
+        )}
+        <Center>
+          <ContactButton secondary type="submit" disabled={isSubmitting}>
+            Send
+          </ContactButton>
+        </Center>
+      </Form>
+    )}
+  </Formik>
+  </div>
+  <Art>
+  <img src={Contact} alt="Contact image" />
+  </Art>
+  </StyledContainer>
+</Section>
 
-        <div className='field'>
-          <label className='label'>Email</label>
-          <div className='control'>
-            <Field className='input' type='email' placeholder='Email' name='email' id='email' />
-          </div>
-          {touched.email && errors.email && <small className='has-text-danger'>{errors.email}</small>}
-        </div>
-
-        <div className='field'>
-          <label className='label'>Message</label>
-          <div className='control'>
-            <Field className='textarea' component='textarea' placeholder='Message' name='message' id='message' rows='6' />
-          </div>
-          {touched.message && errors.message && <small className='has-text-danger'>{errors.message}</small>}
-        </div>
-
-        <div className='field is-grouped is-pulled-right'>
-          <div className='control'>
-            <button className='button' type='reset'>Clear</button>
-          </div>
-          <div className='control'>
-            <button className='button is-primary' type='submit' disabled={isSubmitting}>Submit</button>
-          </div>
-        </div>
-      </form>)}
-    />
-  )
-}
-
-export default ContactForm;
+);
 
 
 
-const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Name is Required!'),
-  email: Yup.string()
-    .email('Enter a Valid Email!')
-    .required('Email is Required!'),
-  message: Yup.string()
-    .required('Message is Required!'),
-})
+const Input = styled.input`
+  display: flex;
+  width: 180%;
+  box-sizing: border-box;
+  border: 2px solid #6c63ff;
+  padding: 0.8rem 1rem;
+  border-radius: 7px;
+  margin-bottom: 0.5rem;
+  transition: 0.3s;
+  ${({ error }) =>
+    error &&
+    `
+		border-color: #BE1818;
+	`}
+  &::placeholder {
+    color: #a7a7a7;
+  }
+`;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    justify-content: center;
+  }
+`;
+
+ const Error = styled.span`
+  color: #BE1818;
+`;
+
+ const Center = styled.div`
+  text-align: left;
+  h4 {
+    font-weight: normal;
+  }
+`;
+
+ const InputField = styled.div`
+  position: relative;
+  margin-bottom: 1rem;
+  color: #000000 ;
+`;
+
+const ContactButton = styled.button`
+  cursor: pointer;
+  border-radius: 3px;
+  padding: 0.7rem 2.5rem;
+  border: none;
+  -webkit-appearance: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #fff;
+  background: #0074d9;
+  &:focus {
+    outline: none;
+  }
+  &:disabled {
+    background: gray;
+  }
+  ${({ secondary }) =>
+    secondary &&
+    `
+		background: #001F3F;
+	`}
+`;
+
+
+
+const Art = styled.figure`
+  width: 600px;
+  position: absolute;
+  top: 20%;
+  right: 50%;
+
+  @media (max-width: ${props => props.theme.screen.lg}) {
+    top: 0;
+    right: 65%;
+    width: 500px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    display: none;
+  }
+`;
